83dbe6022f67cafe94f9390a49e762454b4386b1
jnino46 <jadolnimay26@hotmail.com>
2019-11-26 15:54:22 -0500

    initial commit2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iteracion />
+    </div>
+  );
+}
+
+export default App;
